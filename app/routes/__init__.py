7f83000126847fb6ef87805da7991e1dabbceae9
Sanskar Chourasiya <sanskarchourasiya9179@gmail.com>
2026-02-14 17:33:11 +0530

    Add __all__ to __init__.py for route exports

--- app/routes/__init__.py
+++ app/routes/__init__.py
@@ -1,3 +1,5 @@
 from .patients import router as patients_router
 from .statistics import router as statistics_router
 from .search import router as search_router
+
+__all__ = ['patients_router', 'statistics_router', 'search_router']
